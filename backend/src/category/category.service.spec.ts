5f8e4c1bacaeb7a7228898b51c47332ada456820
Narendra Singh <narendrasinghks2019@gmail.com>
2025-07-29 00:39:46 +0530

    feat: added test cases for category service

--- backend/src/category/category.service.spec.ts
+++ backend/src/category/category.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Category } from './category.schema';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
+  const mockCategoryModel = {
+    create: jest.fn().mockResolvedValue({ name: 'Backend' }),
+    find: jest.fn().mockReturnValue({
+      exec: jest.fn().mockResolvedValue([{ name: 'Backend' }]),
+    })
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+      providers: [
+        CategoryService,
+        {
+          provide: getModelToken(Category.name),
+          useValue: mockCategoryModel,
+        }
+      ],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
@@ -14,5 +29,17 @@ describe('CategoryService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a category', async () => {
+    const result = await service.create({ name: 'Backend' });
+    expect(result.name).toBe('Backend');
+    expect(mockCategoryModel.create).toHaveBeenCalledWith({ name: 'Backend' })
+  });
+
+  it('should return all categories', async () => {
+    const categories = await service.findAll();
+    expect(categories).toEqual([{ name: 'Backend' }]);
+    expect(mockCategoryModel.find).toHaveBeenCalled();
   });
 });
